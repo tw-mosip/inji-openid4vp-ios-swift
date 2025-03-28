d2889faa28dc9170b04b8110eb1419935291e900
adityankannan-tw <adityan.kannan@thoughtworks.com>
2025-03-28 11:23:11 +0530

    [INJIMOB-2885]: fix sonar report coverage generation
    Signed-off-by: adityankannan-tw <adityan.kannan@thoughtworks.com>

--- Package.swift
+++ Package.swift
@@ -5,7 +5,6 @@ import PackageDescription
 let package = Package(
     name: "OpenID4VP",
     platforms: [
-        .macOS(.v12),
         .iOS(.v14)
     ],
     products: [
@@ -25,6 +24,6 @@ let package = Package(
             ]),
         .testTarget(
             name: "OpenID4VPTests",
-            dependencies: ["OpenID4VP"]),
+            dependencies: ["OpenID4VP","jose-swift","Alamofire"]),
     ]
 )
