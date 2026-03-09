b5a94f06842267cbfbeb16045741b0fb5d54e840
KiruthikaJeyashankar <kiruthikavjshankar@gmail.com>
2026-03-09 16:04:29 +0530

    [INJIMOB-3568] refactor: udpate context to v2
    Signed-off-by: KiruthikaJeyashankar <kiruthikavjshankar@gmail.com>

--- Sources/OpenID4VP/AuthorizationResponse/UnsignedVPToken/types/LdpVP/UnsignedLdpVPTokenBuilder.swift
+++ Sources/OpenID4VP/AuthorizationResponse/UnsignedVPToken/types/LdpVP/UnsignedLdpVPTokenBuilder.swift
@@ -26,7 +26,8 @@ public class UnsignedLdpVPTokenBuilder: UnsignedVPTokenBuilder {
     }
     
     func build(credentialInputDescriptorMappings: inout [CredentialInputDescriptorMapping]) async throws -> (vpTokenSigningPayload: VPTokenSigningPayload?, unsignedVPToken: any UnsignedVPToken) {
-        var context: [String] = ["https://www.w3.org/2018/credentials/v1"]
+//        var context: [String] = ["https://www.w3.org/2018/credentials/v1"]
+        var context: [String] = ["https://www.w3.org/ns/credentials/v2"]
         if signatureSuite == SignatureAlgorithm.ed25519Signature2020.rawValue {
             context.append("https://w3id.org/security/suites/ed25519-2020/v1")
         } else if signatureSuite == SignatureAlgorithm.jsonWebSignature2020.rawValue {
